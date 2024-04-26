0ce35e0f8c257c43d5f6e9139c0491da624d08ea
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-04-26 12:46:55 +0530

    second

---
+++ Stop Watch/stop watch/src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import Stopwatch from './Stopwatch.jsx'; // Adjust the import statement and component name
+
+function App() {
+  return (
+    <>
+      <Stopwatch /> 
+    </>
+  );
+}
+
+export default App;
